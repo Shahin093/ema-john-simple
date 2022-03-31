9269a8f691958634aa59fdfef0580d8295012770
Shidul Islam <sishahin093@gmail.com>
2022-04-01 02:22:52 +0600

    53-6 Implement Review Item style with a lot of flex

--- src/components/ReviewItem/ReviewItem.js
+++ src/components/ReviewItem/ReviewItem.js
@@ -1,12 +1,27 @@
 import React from 'react';
-
+import './ReviewItem.css'
 const ReviewItem = (props) => {
-    const { id, name, shipping, quantity, price } = props.product;
+
+    const { id, name, shipping, quantity, price, img } = props.product;
     return (
-        <div>
-            <h2>{name}</h2>
+        <div className='review-item'>
+            <div >
+                <img src={img} alt="" />
+            </div>
+            <div className="review-item-details-container">
+                <div className="review-item-details">
+                    <p title={name} className="product-name">{name.length > 20 ? name.slice(0, 20) + '...' : name}</p>
+                    <p><small>price <span>{price}</span></small></p>
+                    <p><small>Shipping : {shipping}</small></p>
+                    <p><small>Quantity:{quantity}</small></p>
+                </div>
+                <div className="delete-container">
+                    <button>Delete</button>
+                </div>
+            </div>
         </div>
     );
+
 };
 
 export default ReviewItem;
